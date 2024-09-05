ec6ba7f8f754d6954a7a01bb73a968ca8cce1f9e
IceRinne <1024622242@qq.com>
2024-09-05 20:35:13 +0800

    Initial commit

---
+++ src/main/java/com/scmoanno/scmoanno/ScMoAnnoApplication.java
@@ -0,0 +1,13 @@
+package com.scmoanno.scmoanno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScMoAnnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScMoAnnoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scmoanno/scmoanno/ScMoAnnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scmoanno.scmoanno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScMoAnnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
