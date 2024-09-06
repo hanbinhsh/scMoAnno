ed293e8e0060b70fe104f5aa9ecca0458bced515
IceRinne <1024622242@qq.com>
2024-09-06 23:33:49 +0800

    database

--- database/tables.sql
+++ database/tables.sql
@@ -9,3 +9,15 @@ create table `scMoAnnoUser`(
   `is_admin` boolean DEFAULT false	 			NOT NULL	COMMENT '是否是管理员',
   `phone` varchar(32) UNIQUE				 	NOT NULL	COMMENT '电话号码'
 );
+
+drop table if exists `scMoAnnoTask`;
+create table `scMoAnnoTask`(
+  `task_id` int AUTO_INCREMENT PRIMARY KEY  	NOT NULL	COMMENT '任务ID',
+  `task_name` varchar(20) UNIQUE				NOT NULL	COMMENT '任务名',
+  `start_time` datetime				 			NOT NULL	COMMENT '开始时间',
+  `end_time` datetime				 			NOT NULL	COMMENT '结束时间',
+  `status` tinyint DEFAULT 0					NOT NULL	COMMENT '标志位',
+  `details` text								NOT NULL	COMMENT '详情',
+  `uploader_id` int 							NOT NULL	COMMENT '上传者ID',
+  FOREIGN KEY (`uploader_id`) REFERENCES scMoAnnoUser(`user_id`)
+);
