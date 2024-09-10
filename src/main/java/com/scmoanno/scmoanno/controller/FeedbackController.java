ca3a6de6022b77428ff820720723bc2774b3e3de
aweiawei666 <2115409491@qq.com>
2024-09-10 22:25:59 +0800

    feedback

--- src/main/java/com/scmoanno/scmoanno/controller/FeedbackController.java
+++ src/main/java/com/scmoanno/scmoanno/controller/FeedbackController.java
@@ -4,11 +4,11 @@ import com.scmoanno.scmoanno.entity.Feedback;
 import com.scmoanno.scmoanno.entity.Result;
 import com.scmoanno.scmoanno.servers.FeedbackServer;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.PublicKey;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class FeedbackController {
@@ -18,6 +18,23 @@ public class FeedbackController {
     @PostMapping("/feedback")
     public Result<String> feedback(@RequestBody Feedback feedback) {
         feedbackServer.insert(feedback);
+        return Result.success();
+    }
+
+    @GetMapping("/findFeedback")
+    public Result<List<Feedback>> getFeedback() {
+        return Result.success(feedbackServer.getFeedback());
+    }
+
+    @RequestMapping("/findAllFeedbackWithUserInformation")
+    @CrossOrigin(origins = "*")
+    public Result<Map<Object,Object>> findAllFeedbackWithUserInformation(){
+        return Result.success(feedbackServer.findAllFeedbackWithUserInformation());
+    }
+
+    @DeleteMapping("/deleteFeedback/{feedbackId}")
+    public Result<String> deleteFeedback(@PathVariable long feedbackId) {
+        feedbackServer.deleteFeedback(feedbackId);
         return Result.success();
     }
 }
