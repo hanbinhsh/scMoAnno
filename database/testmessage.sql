e8ca00431e39debb68c9994d621bdcf631a681f6
IceRinne <1024622242@qq.com>
2024-09-12 23:14:58 +0800

    user

--- database/testmessage.sql
+++ database/testmessage.sql
@@ -12,3 +12,26 @@ INSERT INTO `scMoAnnoTask` (`task_name`, `start_time`, `end_time`, `status`, `de
 ('Annotation Task 3', '2024-09-05 12:00:00', '2024-09-06 14:00:00', 0, 'Details about Task 3', 1),
 ('Annotation Task 4', '2024-09-07 15:00:00', '2024-09-08 16:30:00', 2, 'Details about Task 4', 2);
 
+INSERT INTO `scMoAnnoTask` (`task_name`, `start_time`, `end_time`, `status`, `details`, `uploader_id`) VALUES
+('Annotation Task 5', '2024-09-01 10:00:00', '2024-09-02 10:00:00', 0, 'Details about Task 1', 1),
+('Annotation Task 6', '2024-09-03 09:00:00', '2024-09-04 11:00:00', 1, 'Details about Task 2', 1),
+('Annotation Task 7', '2024-09-05 12:00:00', '2024-09-06 14:00:00', 0, 'Details about Task 3', 1),
+('Annotation Task 8', '2024-09-07 15:00:00', '2024-09-08 16:30:00', 2, 'Details about Task 4', 2);
+
+INSERT INTO `scMoAnnoTask` (`task_name`, `start_time`, `end_time`, `status`, `details`, `uploader_id`) VALUES
+('Annotation Task 9', '2024-09-01 10:00:00', '2024-09-02 10:00:00', 0, 'Details about Task 1', 1),
+('Annotation Task 10', '2024-09-03 09:00:00', '2024-09-04 11:00:00', 1, 'Details about Task 2', 1),
+('Annotation Task 11', '2024-09-05 12:00:00', '2024-09-06 14:00:00', 0, 'Details about Task 3', 1),
+('Annotation Task 12', '2024-09-07 15:00:00', '2024-09-08 16:30:00', 2, 'Details about Task 4', 2);
+
+INSERT INTO `scMoAnnoTask` (`task_name`, `start_time`, `end_time`, `status`, `details`, `uploader_id`) VALUES
+('Annotation Task 13', '2024-09-01 10:00:00', '2024-09-02 10:00:00', 0, 'Details about Task 1', 1),
+('Annotation Task 14', '2024-09-03 09:00:00', '2024-09-04 11:00:00', 1, 'Details about Task 2', 1),
+('Annotation Task 15', '2024-09-05 12:00:00', '2024-09-06 14:00:00', 0, 'Details about Task 3', 1),
+('Annotation Task 16', '2024-09-07 15:00:00', '2024-09-08 16:30:00', 2, 'Details about Task 4', 2);
+
+INSERT INTO `scMoAnnoTask` (`task_name`, `start_time`, `end_time`, `status`, `details`, `uploader_id`) VALUES
+('Annotation Task 17', '2024-09-01 10:00:00', '2024-09-02 10:00:00', 0, 'Details about Task 1', 1),
+('Annotation Task 18', '2024-09-03 09:00:00', '2024-09-04 11:00:00', 1, 'Details about Task 2', 1),
+('Annotation Task 19', '2024-09-05 12:00:00', '2024-09-06 14:00:00', 0, 'Details about Task 3', 1),
+('Annotation Task 20', '2024-09-07 15:00:00', '2024-09-08 16:30:00', 2, 'Details about Task 4', 2);
