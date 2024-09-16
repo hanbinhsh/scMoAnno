3059acf74b7c06e86da7df4f1378c9485cc13f5c
IceRinne <1024622242@qq.com>
2024-09-16 14:40:48 +0800

    user

--- src/main/java/com/scmoanno/scmoanno/entity/Scmoannouser.java
+++ src/main/java/com/scmoanno/scmoanno/entity/Scmoannouser.java
@@ -1,7 +1,6 @@
 package com.scmoanno.scmoanno.entity;
 
 public class Scmoannouser {
-
   private long userId;
   private String userName;
   private String psw;
@@ -11,74 +10,46 @@ public class Scmoannouser {
   private byte[] avatar;
   private String avatarBase64; // 头像的 Base64 编码字符串
 
-
   public long getUserId() {
     return userId;
   }
-
   public void setUserId(long userId) {
     this.userId = userId;
   }
-
-
   public String getUserName() {
     return userName;
   }
-
   public void setUserName(String userName) {
     this.userName = userName;
   }
-
-
   public String getPsw() {
     return psw;
   }
-
   public void setPsw(String psw) {
     this.psw = psw;
   }
-
-
   public String getEmail() {
     return email;
   }
-
   public void setEmail(String email) {
     this.email = email;
   }
-
-
   public long getIsAdmin() {
     return isAdmin;
   }
-
   public void setIsAdmin(long isAdmin) {
     this.isAdmin = isAdmin;
   }
-
-
   public String getPhone() {
     return phone;
   }
-
   public void setPhone(String phone) {
     this.phone = phone;
   }
-
-
   public byte[] getAvatar() {
     return avatar;
   }
-
-  public void setAvatar(byte[] avatar) {
-    this.avatar = avatar;
-  }
-
-    public String getAvatarBase64() {
-        return avatarBase64;
-    }
-
-    public void setAvatarBase64(String avatarBase64) {
-        this.avatarBase64 = avatarBase64;
-    }
+  public void setAvatar(byte[] avatar) {this.avatar = avatar;}
+  public String getAvatarBase64() {return avatarBase64;}
+  public void setAvatarBase64(String avatarBase64) {this.avatarBase64 = avatarBase64;}
 }
